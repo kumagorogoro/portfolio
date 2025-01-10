dd3956f2945ec850dbb16476099d8fe6b9e68aaf
kumagorogoro <marustage.ooo@gmail.com>
2025-01-10 23:19:43 +0900

    footer slider改善

--- js/works.js
+++ js/works.js
@@ -53,16 +53,22 @@ $(function () {
     autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
     speed: 7000, //スライドが流れる速度を設定
     cssEase: "linear", //スライドの流れ方を等速に設定
-    slidesToShow: 6, //表示するスライドの数
+    slidesToShow: 12, //表示するスライドの数
     swipe: false, // 操作による切り替えはさせない
     arrows: false, //矢印非表示
     pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
     pauseOnHover: false, //スライダーにマウスホバーした時にスライドを停止させるか
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 6, // 1100px以下でスライド6枚表示
+        },
+      },
+      {
         breakpoint: 750,
         settings: {
-          slidesToShow: 3, //画面幅750px以下でスライド3枚表示
+          slidesToShow: 3, // 750px以下でスライド3枚表示
         },
       },
     ],
@@ -75,7 +81,7 @@ $(function () {
     autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
     speed: 7000, // スライドが流れる速度を設定
     cssEase: "linear", // スライドの流れ方を等速に設定
-    slidesToShow: 6, // 表示するスライドの数
+    slidesToShow: 12, // 表示するスライドの数
     swipe: false, // 操作による切り替えはさせない
     arrows: false, // 矢印非表示
     pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
@@ -83,9 +89,15 @@ $(function () {
     rtl: true, // スライダーを左から右に流す（逆向き）
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 6, // 1100px以下でスライド6枚表示
+        },
+      },
+      {
         breakpoint: 750,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 3, // 750px以下でスライド3枚表示
         },
       },
     ],
