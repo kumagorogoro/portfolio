6c80d502b030c71ef630544153754081b9e781a0
kumagorogoro <marustage.ooo@gmail.com>
2025-01-20 20:23:07 +0900

    js追加

--- js/titleleft.js
+++ js/titleleft.js
@@ -9,12 +9,4 @@ function checkScroll() {
       .querySelector(".half-circle-text")
       .classList.add("start-animation");
   }
-  const element2 = document.querySelector(".half-circle-wrapper2");
-  const position2 = element2.getBoundingClientRect();
-  if (position2.top < window.innerHeight && position2.bottom >= 0) {
-    document
-      .querySelector(".half-circle-text2")
-      .classList.add("start-animation2");
-  }
 }
-
---
+++ js/titleright.js
@@ -0,0 +1,7 @@
+const element2 = document.querySelector(".half-circle-wrapper2");
+const position2 = element2.getBoundingClientRect();
+if (position2.top < window.innerHeight && position2.bottom >= 0) {
+  document
+    .querySelector(".half-circle-text2")
+    .classList.add("start-animation2");
+}
